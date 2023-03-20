d2899212e7696640402c1a808503947bb7eb7d8c
Bryan <soloudithz@gmail.com>
2023-03-20 06:56:18 -0400

    Update Euler Problem 7- 10001st Prime.py
    I removed the unused ("commented out") code from the initial commit but left in the code that times the program. The program takes approximately 20 seconds to calculate the 10,001st prime.

--- Euler Problem 7- 10001st Prime.py
+++ Euler Problem 7- 10001st Prime.py
@@ -5,21 +5,11 @@ import time
 start_time = time.time() # to track how long the program takes to run.
 solution = 13
 count = 6
-#list_of_primes = [2, 3, 5, 7, 11, 13]
 
 def is_prime(num):
-    #for prime in list_of_primes:
-        #if prime **2 <= num:
-            #if num % prime == 0:
-                #print(num, ' is divisible by ', prime, '.', sep='')
-                #return False
     for i in range(3, num, 2):
         if num % i == 0:
-            #print(num, ' is divisible by ', i, '.', sep='')
             return False
-    #list_of_primes.append(num)
-    #print(str(num), 'is prime.')
-    #print(list_of_primes)
     return True
 
 while count < 10001:

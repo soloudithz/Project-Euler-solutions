b3bf744638ec3b269b7c47cf656c498cfaf5bfd5
Bryan <soloudithz@gmail.com>
2023-04-19 08:25:55 -0400

    Create Euler Problem 17- Number Letter Counts.py
    This solution leverages the num2words module. (More info at: https://pypi.org/project/num2words/)
    
    The user provides a maximum number, n. For each integer from 1 to that number (inclusive), num2words converts that integer into a string and concatenates it to a string_of_nums. Then, all spaces and hyphens are removed, leaving us with a string of letters, the length of which is printed.
    
    Benchmarks:
    *At the time of uploading to GitHub, my machine computes the standard test case of all numbers 1 to 1,000 in about 0.099 seconds.
    *1 to 10,000 is solved in 0.49 seconds.
    *1 to 100,000 is solved in 6.06 seconds.

---
+++ Euler Problem 17- Number Letter Counts.py
@@ -0,0 +1,20 @@
+# Project Euler -- Promblem 17: Number Letter Counts
+# https://projecteuler.net/problem=17
+
+from num2words import num2words
+import time
+
+n = int(input('The sum of all letters in all numbers up to '))
+
+start_time = time.time()   # to track how long the program takes to run
+
+string_of_nums = ''
+
+for i in range(n):
+    string_of_nums += num2words(i)
+for character in ('-', ' '):
+    string_of_nums = string_of_nums.replace(character, '')
+#print(string_of_nums)
+
+print('is {output:,}.'.format(output=len(string_of_nums)))
+print('This program took', time.time() - start_time, 'seconds to run.')

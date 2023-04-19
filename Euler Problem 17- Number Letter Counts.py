069e448b249e6b524e32838b375f0abef99ad811
Bryan <soloudithz@gmail.com>
2023-04-19 08:29:02 -0400

    Update Euler Problem 17- Number Letter Counts.py
    Minor text update

--- Euler Problem 17- Number Letter Counts.py
+++ Euler Problem 17- Number Letter Counts.py
@@ -4,13 +4,13 @@
 from num2words import num2words
 import time
 
-n = int(input('The sum of all letters in all numbers up to '))
+n = int(input('The sum of all letters in all numbers up to (and including) '))
 
 start_time = time.time()   # to track how long the program takes to run
 
 string_of_nums = ''
 
-for i in range(n):
+for i in range(1, n+1):
     string_of_nums += num2words(i)
 for character in ('-', ' '):
     string_of_nums = string_of_nums.replace(character, '')
